a2a7ffafe01f718c912cbda26d2711dd909f8a6c
lijiankun24 <jiankunli24@gmail.com>
2020-02-06 21:06:51 +0800

    ADD: animation_demo1

---
+++ lib/animation/animation_demo1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+/// Flutter 中的动画：https://flutterchina.club/animations/
+class Constraint extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
