d74a5da686801f58cdce97f361d0cdad00df9a45
lijiankun24 <jiankunli24@gmail.com>
2020-02-14 21:07:27 +0800

    ADD: GestureDemo

---
+++ lib/gesture/GestureDemo.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GestureDemo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('GestureDemo'),
+        ),
+        body: Center(
+          child: Container(
+            child: ,
+          ),
+        ),
+      ),
+    );
+  }
+}
