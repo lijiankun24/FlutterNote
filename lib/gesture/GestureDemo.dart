10ddd7c009c80225365d398dcbc7448e78d314ad
lijiankun24 <jiankunli24@gmail.com>
2020-02-15 20:56:12 +0800

    ADD: GestureDemo 100%

--- lib/gesture/GestureDemo.dart
+++ lib/gesture/GestureDemo.dart
@@ -9,12 +9,75 @@ class GestureDemo extends StatelessWidget {
         appBar: AppBar(
           title: Text('GestureDemo'),
         ),
-        body: Center(
+        body: GestureLogo(),
+      ),
+    );
+  }
+}
+
+class GestureLogo extends StatefulWidget {
+
+  @override
+  _GestureState createState() => _GestureState();
+}
+
+class _GestureState extends State<GestureLogo> {
+  double top = 0.0;
+  double left = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        GestureDetector(
+          behavior: HitTestBehavior.translucent,
           child: Container(
-            child: ,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
+            height: MediaQuery
+                .of(context)
+                .size
+                .height,
+            color: Colors.yellow,
+          ),
+          onPanDown: (details) {
+            setState(() {
+              top = details.globalPosition.dy;
+              left = details.globalPosition.dx;
+            });
+          },
+          onPanUpdate: (details) {
+            setState(() {
+              top = details.globalPosition.dy;
+              left = details.globalPosition.dx;
+            });
+          },
+          onPanCancel: () {
+            setState(() {
+              top = 0;
+              left = 0;
+            });
+          },
+          onPanEnd: (details) {
+            setState(() {
+              top = 0;
+              left = 0;
+            });
+          },
+        ),
+        Positioned(
+          left: left,
+          top: top,
+          child: Container(
+            width: 100.0,
+            height: 100.0,
+            color: Colors.green,
           ),
         ),
-      ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'animation/animation_demo5.dart';
+import 'gesture/GestureDemo.dart';
 
 void main() {
-  runApp(AnimationDemo());
+  runApp(GestureDemo());
 
   if (Platform.isAndroid) {
     SystemChrome.setSystemUIOverlayStyle(
